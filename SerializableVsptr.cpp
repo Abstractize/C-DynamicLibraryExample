feb2b21e5ecd029338615c6458e3f58fca4f2233
Gabriel Abarca Aguilar <gababarca@hotmail.com>
2020-05-16 18:27:00 -0600

    [Fix] Library

--- SerializableVsptr.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Gabriel Abarca Aguilar on 5/8/20.
-//
-
-#include "SerializableVsptr.h"
--- library.h
+++ library.h
@@ -1,13 +1,12 @@
 #include "Person.h"
 
-#ifndef PAPITASBBQ __attribute__((visibility("default")));
+#ifndef  __attribute__((visibility("default")));
 #define PAPITASBBQ
 
 extern "C"
 {
     //Funciones que se llaman en JS
     int CountPeople();
-    void StartGB();
 };
 
 
